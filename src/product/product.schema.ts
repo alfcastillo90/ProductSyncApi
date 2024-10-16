af242003c7156da1aa1232d1dab42ef9f2ccb98c
Carlos Alfredo Castillo Rodriguez <cacr1990@gmail.com>
2024-10-16 17:15:12 -0300

    Refactor Product schema to add soft delete functionality

--- src/product/product.schema.ts
+++ src/product/product.schema.ts
@@ -4,24 +4,26 @@ export interface Product extends Document {
   sku: string;
   name: string;
   brand: string;
-  productModel: string; // Renombramos el campo para evitar conflictos
+  productModel: string;
   category: string;
   color: string;
   price: number;
   currency: string;
   stock: number;
   createdAt: Date;
+  deletedAt?: Date | null; // Campo para soft delete
 }
 
 export const ProductSchema = new Schema<Product>({
   sku: { type: String, required: true },
   name: { type: String, required: true },
   brand: { type: String, required: true },
-  productModel: { type: String, required: true }, // Aquí también lo renombramos
+  productModel: { type: String, required: true },
   category: { type: String, required: true },
   color: { type: String, required: true },
   price: { type: Number, required: true },
   currency: { type: String, required: true },
   stock: { type: Number, required: true },
   createdAt: { type: Date, default: Date.now },
+  deletedAt: { type: Date, default: null }, // Soft delete
 });
