35e16b2c33fa5f5890aa7219150ec5c215121a72
Carlos Alfredo Castillo Rodriguez <cacr1990@gmail.com>
2024-10-16 18:53:08 -0300

    Refactor ProductController to remove syncProductsPostgres endpoint

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -8,7 +8,6 @@ import { ApiOperation, ApiTags } from '@nestjs/swagger';
 export class ProductController {
   constructor(
     private readonly productService: ProductService,
-    private readonly productPostgresService: ProductPostgresService,
   ) {}
 
   @ApiOperation({ summary: 'Sync products from Contentful to MongoDB' })
@@ -16,14 +15,6 @@ export class ProductController {
   async syncProducts(): Promise<{ message: string }> {
     await this.productService.fetchProducts(); // Sincroniza con MongoDB
     return { message: 'OK' }; // Devuelve un OK después de ejecutar la función
-  }
-
-  @ApiTags('Products Postgres')
-  @ApiOperation({ summary: 'Sync products from Contentful to PostgreSQL' })
-  @Get('sync-postgres')
-  async syncProductsPostgres(): Promise<{ message: string }> {
-    await this.productPostgresService.fetchAndSaveProductsInPostgres(); // Sincroniza con PostgreSQL
-    return { message: 'OK' };
   }
 
   @ApiOperation({ summary: 'Get paginated products' })
